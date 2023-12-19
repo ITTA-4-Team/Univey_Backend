7755dfb31484b6716650726bf0c22363721951ac
teho <xogh8755@naver.com>
2023-12-19 16:44:26 +0900

    [FEAT]#7 user api endpoint users로 변경

--- src/main/java/ita/univey/domain/user/domain/controller/UserController.java
+++ src/main/java/ita/univey/domain/user/domain/controller/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
     private final KakaoService kakaoService;
