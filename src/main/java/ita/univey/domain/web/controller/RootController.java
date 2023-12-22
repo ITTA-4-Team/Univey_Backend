6d94d5ecc741dbbfdf3710cc4d7df1dec84445ba
LeeJaeHyeok97 <hazardous10@naver.com>
2023-12-23 05:42:35 +0900

    [FIX]:#22 CD test

--- src/main/java/ita/univey/domain/web/controller/RootController.java
+++ src/main/java/ita/univey/domain/web/controller/RootController.java
@@ -1,11 +1,14 @@
 package ita.univey.domain.web.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/health")
     public String healthCheck() {
         return "I'm healthy!!!";
