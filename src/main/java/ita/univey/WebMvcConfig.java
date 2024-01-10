2e4ba597740434b42603c8dc571cbe190122f142
teho <xogh8755@naver.com>
2024-01-10 17:25:17 +0900

    [FEAT]#22 cors해결을 위한 webmvcconfig 활성화

---
+++ src/main/java/ita/univey/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package ita.univey;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .maxAge(3600)
+                .allowCredentials(true);
+
+    }
+}
