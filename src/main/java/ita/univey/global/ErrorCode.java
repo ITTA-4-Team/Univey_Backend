9643dac31ecbdbad0aaeba2626ecb881161808d8
teho <xogh8755@naver.com>
2024-01-11 18:16:59 +0900

    [FIX] ci/cd 테스트

--- src/main/java/ita/univey/global/ErrorCode.java
+++ src/main/java/ita/univey/global/ErrorCode.java
@@ -3,13 +3,14 @@ package ita.univey.global;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+
 import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
     // api 만들고 수정하기
-    REQUEST_VALIDATION_EXCEPTION(BAD_REQUEST, "잘못된 요청입니다."),
+    REQUEST_VALIDATION_EXCEPTION(BAD_REQUEST, "잘못된 요청입니다!."),
     EXPIRED_TOKEN(UNAUTHORIZED, "만료된 토큰입니다."),
     DUPLICATE_PARTICIPATION(CONFLICT, "중복 참여입니다.");
 
