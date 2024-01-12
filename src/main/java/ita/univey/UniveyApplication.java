b736f2910d602292170a8f70a29bb0d49f700374
teho <xogh8755@naver.com>
2024-01-12 15:27:50 +0900

    [FIX]ec2에서 운영중인 스프링 한국 시간대로 변경

--- src/main/java/ita/univey/UniveyApplication.java
+++ src/main/java/ita/univey/UniveyApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class UniveyApplication {
+    @PostConstruct // ec2 스프링 시간 한국 시간으로 설정.
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UniveyApplication.class, args);
