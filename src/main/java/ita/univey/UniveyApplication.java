9aa52c8feb4a22e7ab8a9a8899f6d73df5ab698e
teho <xogh8755@naver.com>
2024-01-13 11:49:23 +0900

    [FIX]스케줄링 활성화

--- src/main/java/ita/univey/UniveyApplication.java
+++ src/main/java/ita/univey/UniveyApplication.java
@@ -3,10 +3,12 @@ package ita.univey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class UniveyApplication {
